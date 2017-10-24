34df846cdff340a34980bcc8b7f8267d77cbdffc
jingyu9575 <jingyu9575@hotmail.com>
2017-10-24 22:02:42 +0800

    Initial commit

---
+++ user-chrome-js-1/api.js
@@ -0,0 +1,45 @@
+const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components
+Cu.import("resource://gre/modules/Services.jsm")
+
+const file = Services.dirsvc.get("ProfD", Ci.nsIFile)
+file.append('chrome')
+file.append('userChrome.js')
+
+function* getAllWindows(type) {
+	const enumerator = Services.wm.getEnumerator('navigator:browser');
+	while (enumerator.hasMoreElements()) yield enumerator.getNext()
+}
+
+async function injectIntoWindow(window) {
+	try {
+		if (!file.exists() || !file.isFile()) {
+			console.warn('userChrome.js not found')
+			return
+		}
+		if (window.document.readyState !== "complete")
+			await new Promise(resolve => window.addEventListener("load", resolve))
+		const url = 'file://' + file.path
+
+		const sandbox = Cu.Sandbox(window, {
+			sandboxPrototype: window,
+			sameZoneAs: window,
+			wantXrays: false,
+		})
+		const script = await window.ChromeUtils.compileScript(url)
+		script.executeInGlobal(sandbox)
+	} catch (error) { console.error(error) }
+}
+
+[...getAllWindows()].map(injectIntoWindow)
+
+Services.ww.registerNotification({
+	observe(subject, topic) {
+		if (topic !== 'domwindowopened') return
+		const window = subject.QueryInterface(Ci.nsIDOMWindow)
+		void injectIntoWindow(window)
+	},
+})
+
+class API extends ExtensionAPI {
+	getAPI(context) { return { activate() { } } }
+}
